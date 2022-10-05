96afdda271b7ea454da86cba6560d2b4ce56e5ee
name_snrl <Demogorgon-74@ya.ru>
2022-10-05 10:12:34 +0500

    Agent pkg must be as stupid as it possible. Unfortunately, agent pkg can only be used as an image for further copying to working directory. It seems that the launcher and agent libraries reference each other and cannot be separated by a symlink. Also, they cannot be in an immutable file system, like nix store, because the agent's home directory depends on where the launcher libraries are located, and the most problematic thing is that when updating, the agent will try to create update directory in the home, even if symlink to it already exist. Without update, the agent cannot start working.
    `WARN - jetbrains.buildServer.AGENT - Unable to start upgrade: Failed
    to create /nix/store/2b82hz7nk45il3ffcvgpps66dmmyxm9p-teamcity-agent-22.04.4/update`

--- agent_as_pkg.nix
+++ agent_as_pkg.nix
@@ -3,58 +3,15 @@ input: { config, lib, pkgs, ... }:
 with lib;
 
 let
-  stateDirs = [
-    "temp"
-    "logs"
-    "work"
-    "system"
-    "tools"
-    "backup"
-    "update"
-    "launcher/logs"
-  ] ++ existedStateDirs;
-
-  existedStateDirs = [ "plugins" "conf" ];
-
-  additionalOutputs = map (builtins.replaceStrings ["/"] [""]) existedStateDirs;
-
   pkg = pkgs.stdenv.mkDerivation (input // {
-    outputs = [ "out" ] ++ additionalOutputs;
-
-    installPhase = ''
-      ex=(${builtins.concatStringsSep " " existedStateDirs})
-
-      for i in ''${ex[@]}; do
-        dest=''${i/\//}
-        mv buildAgent/$i ''${!dest}
-      done
-
-      cp -R buildAgent $out
-
-      state=(${builtins.concatStringsSep " " stateDirs})
-
-      for i in ''${state[@]}; do
-        rm -rf $out/$i &> /dev/null
-        ln -sf ${dataDir}/$i/ $out/
-      done
-    '';
+    installPhase = "cp -R buildAgent $out";
   });
 
   dataDir = "/var/teamcity/agent";
 
   installer = pkgs.writeShellScriptBin "tc-agent" ''
-    rm -rf ${dataDir}
-    mkdir -p ${dataDir}/{${builtins.concatStringsSep "," stateDirs}}
-
-    cp -rsfP ${pkg}/* ${dataDir}/
-
-    ex=(${lib.concatMapStringsSep " " (p: p + "=${pkg.${p}}") additionalOutputs})
-
-    for i in ''${ex[@]}; do
-      dest=''${i%=*}
-      source=''${i#*=}
-      cp -rsfP $source/* ${dataDir}/$dest/
-    done
+    rm -rf ${dataDir} && mkdir -p ${dataDir}/lib
+    cp -rf ${pkg}/* ${dataDir}/
   '';
 in
 
